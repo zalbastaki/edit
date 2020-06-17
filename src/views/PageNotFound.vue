ee3f8907ef5841059a193fb14e4bd09d7637c5f0
Zain Albastaki <zain.albastaki@gmail.com>
2020-06-17 11:24:33 +0300

    Add page not found

---
+++ src/views/PageNotFound.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="page-not-found">
+        <h1 class="subheading">Oops, page not found.</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'page-not-found',
+    };
+</script>
+
+<style lang="scss" scoped>
+    #page-not-found {
+        min-height: calc(100vh - #{$footer-height});
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+</style>
